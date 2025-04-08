a97021f9777cce5690f78db89174d2de0f309ea6
Ibrahim-Sefa-Saglam <saglamibrahimsefa@gmail.com>
2025-04-08 21:22:05 +0300

    added character input handler

--- Assets/Systems/Character System/Scripts/Movement.cs
+++ Assets/Systems/Character System/Scripts/Movement.cs
@@ -1,72 +1,59 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 8f;
-    public float groundCheckDistance = 0.1f;
+    public float groundCheckDistance = 0.15f;
 
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private bool jumpPressed;
+    public Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
 
+    // This LayerMask will ignore the "Player" layer
+    private LayerMask groundLayerMask;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        // Get all layers except "Player"
+        int thisLayer = gameObject.layer;
+        groundLayerMask = ~(1 << thisLayer);
     }
-
-    private void Update()
+    public void MoveLeft()
     {
-        // Handle left/right turning and movement
-        if (moveInput.x < 0)
-            MoveLeft();
-        else if (moveInput.x > 0)
-            MoveRight();
-
-        // Handle jump input
-        if (jumpPressed)
-        {
-            Jump();
-            jumpPressed = false; // Prevent continuous jumping
-        }
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            jumpPressed = true;
-        }
-    }
-
-    void MoveLeft()
-    {
-        transform.localScale = new Vector3(-1, 1, 1); // Face left
+        transform.localScale = new Vector3(-1, 1, 1);
         rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
     }
 
-    void MoveRight()
+    public void MoveRight()
     {
-        transform.localScale = new Vector3(1, 1, 1); // Face right
+        transform.localScale = new Vector3(1, 1, 1);
         rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
     }
 
-    void Jump()
+    public void StopHorizontalMovement()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+    }
+    public void Jump()
     {
         Vector2 origin = transform.position;
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance + 0.1f);
+        float rayStartOffset = boxCollider.bounds.extents.y;
+        float rayLength = rayStartOffset + groundCheckDistance;
 
-        Debug.DrawRay(origin, Vector2.down * (groundCheckDistance + 0.1f), Color.red, 0.1f);
+        Vector2 rayStart = origin;
+        Vector2 rayEnd = origin + Vector2.down * rayLength;
 
-        if (hit.collider != null)
+        // Use LayerMask to ignore "Player"
+        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, rayLength, groundLayerMask);
+        Debug.DrawLine(rayStart, rayEnd, Color.red, 1f);
+
+        if (hit.collider)
         {
+            if(hit.collider.gameObject == gameObject) return;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
