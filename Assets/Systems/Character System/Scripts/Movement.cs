1892048bd305d3d6726f0b4d2973404453569aff
Ibrahim-Sefa-Saglam <saglamibrahimsefa@gmail.com>
2025-04-09 18:29:54 +0300

    0

--- Assets/Systems/Character System/Scripts/Movement.cs
+++ Assets/Systems/Character System/Scripts/Movement.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Movement : MonoBehaviour
@@ -8,52 +7,59 @@ public class Movement : MonoBehaviour
     public float jumpForce = 8f;
     public float groundCheckDistance = 0.15f;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
-
-    // This LayerMask will ignore the "Player" layer
     private LayerMask groundLayerMask;
+
+    private float moveDirection = 0f; // -1 for left, 1 for right, 0 for idle
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
 
-        // Get all layers except "Player"
         int thisLayer = gameObject.layer;
         groundLayerMask = ~(1 << thisLayer);
     }
+
+    private void Update()
+    {
+        HandleHorizontalMovement();
+    }
+
+    private void HandleHorizontalMovement()
+    {
+        if (moveDirection != 0f)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(moveDirection), 1f, 1f);
+            transform.position += new Vector3(moveDirection * moveSpeed * Time.deltaTime, 0f, 0f);
+        }
+    }
+
     public void MoveLeft()
     {
-        transform.localScale = new Vector3(-1, 1, 1);
-        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        moveDirection = -1f;
     }
 
     public void MoveRight()
     {
-        transform.localScale = new Vector3(1, 1, 1);
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        moveDirection = 1f;
     }
 
     public void StopHorizontalMovement()
     {
-        rb.velocity = new Vector2(0, rb.velocity.y);
+        moveDirection = 0f;
     }
+
     public void Jump()
     {
         Vector2 origin = transform.position;
-        float rayStartOffset = boxCollider.bounds.extents.y;
-        float rayLength = rayStartOffset + groundCheckDistance;
+        float rayLength = boxCollider.bounds.extents.y + groundCheckDistance;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayLength, groundLayerMask);
+        Debug.DrawRay(origin, Vector2.down * rayLength, Color.red, 1f);
 
-        Vector2 rayStart = origin;
-        Vector2 rayEnd = origin + Vector2.down * rayLength;
-
-        // Use LayerMask to ignore "Player"
-        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, rayLength, groundLayerMask);
-        Debug.DrawLine(rayStart, rayEnd, Color.red, 1f);
-
-        if (hit.collider)
+        if (hit.collider && hit.collider.gameObject != gameObject)
         {
-            if(hit.collider.gameObject == gameObject) return;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
